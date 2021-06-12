c30f43a6b1931ef3edb42e3a865e48ce134d2d5e
david <david.ndungu@student.moringaschool.com>
2021-06-13 00:05:34 +0300

    adds initial routes in the route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GithubComponent} from './github/github.component';
+import {ReposComponent } from './repos/repos.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {"path":"home","component":GithubComponent},
+   {"path":"repos","component":ReposComponent},
+   {"pathMatch":"full", "redirectTo":"home","path":""},
+   {path: '**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
